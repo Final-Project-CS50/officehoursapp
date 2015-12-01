ce0aed4a55f8ac34bcb4ceef37fb31ad1eb008c5
Mark Kelsey <markkelsey6@gmail.com>
2015-12-01 11:14:51 -0500

    First attempt at a basic OH App, with UI outline

---
+++ Office Hours App/StudentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentTableViewCell.swift
+//  Office Hours App
+//
+//  Created by Mark Kelsey on 2015-11-30.
+//  Copyright © 2015 Mark Kelsey. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
