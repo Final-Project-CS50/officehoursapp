abb18aa0712899aa7fdb9f77cda3a92671372609
markkelsey6 <markkelsey6@gmail.com>
2015-12-05 13:17:39 -0500

    Final working copy. Launch page and App Icon added. Landing page formatted. Table formatting adjusted. List formatting adjusted. Compatibility confirmed for all generations of iPhone. Also tested on iPad Retina. Only bug remaining: deleting new users from same session.

--- Office Hours App/AppDelegate.swift
+++ Office Hours App/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        sleep(2)
+        
         //copied from parse getting started guide
         Parse.enableLocalDatastore()
         
